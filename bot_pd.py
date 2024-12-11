c2144119aaf6f579ea041c65fe918be461198b80
crazy <crazytianl@gmail.com>
2024-12-12 02:05:14 +0800

    使用内网IP，更新api key

--- bot_pd.py
+++ bot_pd.py
@@ -1,32 +1,10 @@
-from flask import Flask, request, jsonify
-import telegram
+from datetime import datetime, timedelta
+# 获取今天的日期并设置时间为 00:00:00
+today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
 
-# 初始化 Telegram Bot
-BOT_TOKEN = "7601466837:AAHd9g8QJik3kLtjyRDq-OuYD9CcCWKAJR4"  # 替换为你的 Telegram Bot Token
-CHANNEL_ID = "@solanapostalert"  # 或者使用 Chat ID，例如：-100xxxxxxxxxx
-bot = telegram.Bot(token=BOT_TOKEN)
+# 昨天的 00:00:00
+yesterday_start = today - timedelta(days=1)
 
-app = Flask(__name__)
-
-
-@app.route('/send_message', methods=['POST'])
-async def send_message():
-    try:
-        print(request)
-        # 确保 JSON 数据被正确解析
-        data = request.json
-        
-        if not data:
-            return jsonify({'status': 'error', 'message': 'Invalid JSON data'}), 400
-        
-        message = data.get('message')
-
-         # 使用 Telegram Bot 发送消息
-        await bot.send_message(chat_id=CHANNEL_ID, text=message)
-
-        return jsonify({'status': 'success', 'message': 'Message sent successfully!'})
-    except Exception as e:
-        return jsonify({'status': 'error', 'message': str(e)}), 500
-
-if __name__ == '__main__':
-    app.run(port=5000)
+# 转换为时间戳
+yesterday_start_timestamp = int(yesterday_start.timestamp())
+print("昨天 00:00:00 的时间戳（秒级）：", yesterday_start_timestamp,today.timestamp())
