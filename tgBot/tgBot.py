3b3227b95c4e3be15c682f2354e453ffb9e45b6e
crazy <crazytianl@gmail.com>
2025-01-06 15:35:53 +0800

    新增TG机器人

---
+++ tgBot/tgBot.py
@@ -0,0 +1,57 @@
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+import requests  # 用于发送 Telegram API 请求
+
+# 你创建的 Bot Token
+TOKEN = '7601466837:AAHd9g8QJik3kLtjyRDq-OuYD9CcCWKAJR4'
+
+def reply_message(update: Update, context: CallbackContext) -> None:
+    if getattr(update, 'message', None):  # 安全地检查 message
+        print("这是普通消息:", update.message.text)
+
+    elif getattr(update, 'channel_post', None):  # 安全地检查 channel_post
+        print(update.channel_post)
+        sender_chat_username = update.channel_post.sender_chat.username 
+        if sender_chat_username == 'solanapostalert': #特定频道
+            parts = update.channel_post.text.split()
+            if len(parts) == 2:
+                action = parts[0]  # 动作部分
+                identifier = parts[1]  # ID 部分
+                if action == '拉黑':
+                    if put_in_black(identifier):
+                        update.channel_post.reply_text(f"添加成功")
+                    else:
+                        update.channel_post.reply_text(f"添加失败")
+                else:
+                    update.channel_post.reply_text(f"不能识别的命令 {action}")
+            else:
+              update.channel_post.reply_text(f"命令格式不正确")
+
+def put_in_black(address):#拉黑
+    data = {"walletAddress":address,"label":"TG操作"}
+    response = requests.post(
+        "http://hidog.fun/api/exchange-wallets",
+        headers={"Content-Type": "application/x-www-form-urlencoded"},
+        data=data
+    )
+    if response.status_code == 201:        
+      return True
+    return False
+
+def main():
+    """ 启动机器人并监听消息 """
+    updater = Updater(TOKEN)
+
+    dispatcher = updater.dispatcher
+
+    # 设置消息处理器，处理所有的文本消息
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, reply_message))
+
+    # 启动机器人
+    updater.start_polling()
+
+    # 运行直到按 Ctrl+C 停止
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
