557b7f062cecc4f5553dd276fa12ce093d5adbe3
crazy <crazytianl@gmail.com>
2025-02-05 03:45:26 +0800

    fix bugs

--- utils.py
+++ utils.py
@@ -131,6 +131,7 @@ def similarity_check(data,phishing_wallet_enabled,phishing_wallet_count,insider_
     dev_team = []  # 开发团队
     seen_wallet_tags = set()  # 用来记录已经添加过的 wallet_tag_v2
     for item in data:
+        print(f"item:{item['native_transfer']}")
         from_address = item['native_transfer']['from_address']
         name = item['native_transfer']['name']
 
